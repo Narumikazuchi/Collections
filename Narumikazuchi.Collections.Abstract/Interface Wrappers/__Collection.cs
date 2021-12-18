02a0717224f5c8eb3b5ca88d07adac4df269d1b2
Narumikazuchi <euwkamigoroshi@yahoo.de>
2021-12-18 23:05:51 +0100

    Added for internal use.

---
+++ Narumikazuchi.Collections.Abstract/Interface Wrappers/__Collection.cs
@@ -0,0 +1,17 @@
+﻿namespace Narumikazuchi.Collections.Abstract;
+
+internal sealed class __Collection<TElement> : CollectionBase<Int32, TElement>
+{
+    public __Collection() :
+        base()
+    { }
+    public __Collection(IEnumerable<TElement> source) :
+        base()
+    {
+        foreach (TElement element in source)
+        {
+            this.Insert(index: this.Count,
+                        item: element);
+        }
+    }
+}
